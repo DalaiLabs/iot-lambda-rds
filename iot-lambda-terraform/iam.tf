312b683304adea6905adde1a9033ac5489954c7f
Javkhlan Enkhbold <43927902+JavkhlanEnkhbold@users.noreply.github.com>
2025-07-16 20:39:56 +0200

    update lambda permission to timestream

--- iot-lambda-terraform/iam.tf
+++ iot-lambda-terraform/iam.tf
@@ -2,10 +2,10 @@ resource "aws_iam_role" "lambda_exec" {
   name = "lambda_exec_role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
       Principal = {
         Service = "lambda.amazonaws.com"
       }
@@ -23,14 +23,18 @@ resource "aws_iam_role_policy" "lambda_timestream_access" {
   role = aws_iam_role.lambda_exec.id
 
   policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow"
+        Effect = "Allow",
         Action = [
           "timestream:WriteRecords",
-          "timestream:DescribeEndpoints"
-        ]
+          "timestream:DescribeEndpoints",
+          "timestream:DescribeDatabase",
+          "timestream:DescribeTable",
+          "timestream:Select",
+          "timestream:SelectValues"
+        ],
         Resource = "*"
       }
     ]
