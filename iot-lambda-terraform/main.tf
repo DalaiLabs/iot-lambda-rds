06d71fa18e2ee04dabef77aeb1653a63337dd943
Javkhlan Enkhbold <43927902+JavkhlanEnkhbold@users.noreply.github.com>
2025-07-16 12:27:37 +0200

    test pipeline

--- iot-lambda-terraform/main.tf
+++ iot-lambda-terraform/main.tf
@@ -1,17 +1,17 @@
-
 terraform {
+  backend "s3" {
+    bucket         = "dalai-terraform-state-bucket"
+    key            = "iot/lambda.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-  }
-
-  backend "s3" {
-    bucket         = "your-terraform-state-bucket"
-    key            = "iot/lambda.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table"
   }
 }
 
