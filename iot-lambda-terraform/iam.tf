df443fd67b0c0ecb595f4ca542809d81b2eea575
Javkhlan Enkhbold <43927902+JavkhlanEnkhbold@users.noreply.github.com>
2025-07-16 19:30:50 +0200

    add timestream db

--- iot-lambda-terraform/iam.tf
+++ iot-lambda-terraform/iam.tf
@@ -1,4 +1,3 @@
-
 resource "aws_iam_role" "lambda_exec" {
   name = "lambda_exec_role"
 
@@ -17,4 +16,23 @@ resource "aws_iam_role" "lambda_exec" {
 resource "aws_iam_role_policy_attachment" "lambda_basic" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy" "lambda_timestream_access" {
+  name = "lambda_timestream_access"
+  role = aws_iam_role.lambda_exec.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "timestream:WriteRecords",
+          "timestream:DescribeEndpoints"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
--- lambda/handler.py
+++ lambda/handler.py
@@ -1,4 +1,40 @@
+import boto3
+import os
+import json
+from datetime import datetime
+
+DATABASE_NAME = 'iot_telemetry'
+TABLE_NAME = 'iot_data_items'
+timestream = boto3.client('timestream-write')
 
 def lambda_handler(event, context):
-    print("Received event:", event)
-    return {"status": "ok"}
+    for record in event['Records']:
+        payload = json.loads(record['body']) if 'body' in record else json.loads(record['payload'])
+
+        variable = payload.get('Variable')
+        value = str(payload.get('Value'))
+        station = payload.get('StationName', 'unknown')
+        quality = payload.get('QualityCode', 'UNKNOWN')
+
+        try:
+            timestream.write_records(
+                DatabaseName=DATABASE_NAME,
+                TableName=TABLE_NAME,
+                Records=[
+                    {
+                        'Dimensions': [
+                            {'Name': 'Station', 'Value': station},
+                            {'Name': 'Variable', 'Value': variable},
+                            {'Name': 'QualityCode', 'Value': quality}
+                        ],
+                        'MeasureName': variable,
+                        'MeasureValue': value,
+                        'MeasureValueType': 'DOUBLE',
+                        'Time': str(int(datetime.utcnow().timestamp() * 1000)),  # in ms
+                        'TimeUnit': 'MILLISECONDS'
+                    }
+                ]
+            )
+        except Exception as e:
+            print(f"Error writing to Timestream: {e}")
+    return {"statusCode": 200}
