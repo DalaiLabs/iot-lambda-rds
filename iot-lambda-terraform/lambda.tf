0f502f5d607058aecdabd2a3c436e1f1d625316b
Javkhlan Enkhbold <43927902+JavkhlanEnkhbold@users.noreply.github.com>
2025-07-17 11:45:31 +0200

    fix syntax

--- iot-lambda-terraform/lambda.tf
+++ iot-lambda-terraform/lambda.tf
@@ -1,4 +1,4 @@
-# Lambda Function Definition
+# Lambda Function
 resource "aws_lambda_function" "iot_handler" {
   function_name    = "iot_lambda_handler"
   role             = aws_iam_role.lambda_exec.arn
@@ -15,12 +15,11 @@ resource "aws_lambda_function" "iot_handler" {
   }
 }
 
-# IoT Topic Rule to Listen to Both T0 and T1 Topics
-resource "aws_iot_topic_rule" "iot_rule" {
-  name        = "iot_to_lambda"
+# IoT Rule for Topic T0
+resource "aws_iot_topic_rule" "iot_rule_t0" {
+  name        = "iot_to_lambda_t0"
   enabled     = true
-
-  sql         = "SELECT * FROM 'T0' UNION SELECT * FROM 'T1'"
+  sql         = "SELECT * FROM 'T0'"
   sql_version = "2016-03-23"
 
   lambda {
@@ -28,11 +27,30 @@ resource "aws_iot_topic_rule" "iot_rule" {
   }
 }
 
-# Lambda Permission to Allow IoT Rule Invocation
-resource "aws_lambda_permission" "allow_iot" {
-  statement_id  = "AllowExecutionFromIoT"
+resource "aws_lambda_permission" "allow_iot_t0" {
+  statement_id  = "AllowExecutionFromIoTT0"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.iot_handler.function_name
   principal     = "iot.amazonaws.com"
-  source_arn    = aws_iot_topic_rule.iot_rule.arn
+  source_arn    = aws_iot_topic_rule.iot_rule_t0.arn
+}
+
+# IoT Rule for Topic T1
+resource "aws_iot_topic_rule" "iot_rule_t1" {
+  name        = "iot_to_lambda_t1"
+  enabled     = true
+  sql         = "SELECT * FROM 'T1'"
+  sql_version = "2016-03-23"
+
+  lambda {
+    function_arn = aws_lambda_function.iot_handler.arn
+  }
+}
+
+resource "aws_lambda_permission" "allow_iot_t1" {
+  statement_id  = "AllowExecutionFromIoTT1"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.iot_handler.function_name
+  principal     = "iot.amazonaws.com"
+  source_arn    = aws_iot_topic_rule.iot_rule_t1.arn
 }
